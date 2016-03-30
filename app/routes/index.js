33fcf3fc3dec107af38f76b34b973b7218ab4a99
Jeff Seymour and Lauryn Davis <lryndavis@gmail.com>
2016-03-30 11:20:58 -0700

    add comment model

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('post');
+    return Ember.RSVP.hash({
+      comments: this.store.findAll('comment'),
+      posts: this.store.findAll('post')
+    });
   },
   actions: {
     savePost(params) {
